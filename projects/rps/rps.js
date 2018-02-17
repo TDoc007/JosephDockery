c84ada8c9993bbe2dd1b86ee2fcfd5b50e47403e
Joseph Dockery <doc2g00@hotmail.com>
2018-02-17 10:40:22 -0500

    Added h1 to CYOA and started Bookstore

--- projects/rps/rps.js
+++ projects/rps/rps.js
@@ -1,8 +1,45 @@
-var rng = Math.random();
-var computerChoice = "R";
+// var n = 1;
+//
+// while (n <= 10) {
+//     console.log(n);
+//     n++;
+// }
+var userWins = "You win!";
+var computerWins = "The computer wins!";
+var roundCounter = 0;
 
-if(rng > 0.66) {
-    computerChoice = "P";
-} else if (rng > 0.33) {
-    computerChoice = "S";
-}
+var gameRound = function(){
+    var userChoice = prompt("Choose Rock, Paper, or Scissors by typing 'R', 'P', or 'S'");
+    var computerChoice = "R";
+    var rng = Math.random();
+
+    if(rng > 0.66) {
+        computerChoice = "P";
+    } else if (rng > 0.33) {
+        computerChoice = "S";
+    }
+
+    if(computerChoice !== userChoice){
+        if(computerChoice === "R"){
+            if (userChoice === "S") {
+                alert(computerWins);
+            } else {
+                alert(userWins);
+            }
+        } else if (computerChoice === "P") {
+            if( userChoice === "R") {
+                alert(computerWins);
+            } else {
+                alert(userWins);
+            }
+        } else {
+            if( userChoice === "P") {
+                alert(computerWins);
+            } else {
+                alert(userWins);
+            }
+        }
+    } else {
+        alert('Tie!');
+    }
+};
